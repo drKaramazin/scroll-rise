1e008827767b0af828d85a96490c6231f3014444
n.grishkin <n.grishkin@iva.ru>
2022-11-09 21:25:22 +0500

    test fix

--- src/models/motions/spec/move.fixture.ts
+++ src/models/motions/spec/move.fixture.ts
@@ -8,8 +8,8 @@ export abstract class MoveFixture extends MotionFixture {
   static changeXTimeFrame(): TimeFrame {
     return new TimeFrame(new MoveMotion({
       startX: () => 0,
-      endX: (w: number) => w - MotionFixture.block.width,
       startY: () => 0,
+      endX: (w: number) => w - MotionFixture.block.width,
       endY: (w: number, h: number) => 0,
     }), (w: number, h: number) => 0, (w: number, h: number) => Util.innerHeight());
   };
@@ -17,18 +17,18 @@ export abstract class MoveFixture extends MotionFixture {
   static changeYTimeFrame(): TimeFrame {
     return new TimeFrame(new MoveMotion({
       startX: () => 0,
-      endX: (w: number) => 0,
       startY: () => 0,
-      endY: (w: number, h: number) => h - MotionFixture.block.height,
+      endX: (w: number) => 0,
+      endY: (w: number, h: number) => Util.innerHeight() - MotionFixture.block.height,
     }), (w: number, h: number) => 0, (w: number, h: number) => Util.innerHeight());
   }
 
   static changeXYTimeFrame(): TimeFrame {
     return new TimeFrame(new MoveMotion({
       startX: () => 0,
-      endX: (w: number) => w - MotionFixture.block.width,
       startY: () => 0,
-      endY: (w: number, h: number) => h - MotionFixture.block.height,
+      endX: (w: number) => w - MotionFixture.block.width,
+      endY: (w: number, h: number) => Util.innerHeight() - MotionFixture.block.height,
     }), (w: number, h: number) => 0, (w: number, h: number) => Util.innerHeight());
   }
 
