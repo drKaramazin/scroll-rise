499283050a557eaa42ad83026855129bec80e5e6
n.grishkin <n.grishkin@iva.ru>
2022-11-11 12:18:54 +0500

    test

--- src/models/motions/spec/fas-move.fixture.ts
+++ src/models/motions/spec/fas-move.fixture.ts
@@ -75,7 +75,7 @@ export class FasMoveFixture extends MoveFixture {
         },
         coords: {
           x: { value: 0 },
-          y: { value: Util.innerHeight() - MoveFixture.block.height },
+          y: { value: Util.clientHeight() - MoveFixture.block.height },
         },
       }, {
         scrollTo: {
@@ -118,7 +118,7 @@ export class FasMoveFixture extends MoveFixture {
         },
         coords: {
           x: { value: Util.clientWidth() - MoveFixture.block.width },
-          y: { value: Util.innerHeight() - MoveFixture.block.height },
+          y: { value: Util.clientHeight() - MoveFixture.block.height },
         },
       }, {
         scrollTo: {
