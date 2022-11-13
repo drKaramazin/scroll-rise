4e6066c424f550919db76c4bf7190cc6dacc1204
n.grishkin <n.grishkin@iva.ru>
2022-11-13 16:28:46 +0500

    log

--- src/models/scenes/sticky-platform.scene.ts
+++ src/models/scenes/sticky-platform.scene.ts
@@ -20,13 +20,11 @@ export class StickyPlatformScene extends SceneModel<StickyPlatformSceneOptions> 
   }
 
   override resizeHeight(): void {
-    console.log('Set scene height', this.height(Util.clientWidth(), Util.clientHeight()));
     this.el.style.height = `${this.height(Util.clientWidth(), Util.clientHeight())}px`;
     this.resizePlatform();
   }
 
   resizePlatform(): void {
-    console.log('Set platform height', this.options!.stickyPlatformHeight!(Util.clientWidth(), Util.clientHeight()));
     this.platform.style.height = `${this.options!.stickyPlatformHeight!(Util.clientWidth(), Util.clientHeight())}px`;
   }
 
